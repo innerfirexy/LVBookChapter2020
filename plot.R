97177c2803e28aa1c1e440bd6da18033b67ff985
xy@工作間 <innerfirexy@gmail.com>
2020-07-05 21:50:28 -0700

    plot

--- plot.R
+++ plot.R
@@ -16,17 +16,36 @@ d.vsize = fread("wikisource_chn_vocab_size_year.csv")
 # combine
 d.comb = cbind(d.wcount, d.ccount$charCount, d.vsize$vocabSize) %>%
     rename(charCount = V2, vocabSize = V3)
-
 d.combm = melt(d.comb, id.vars = "year", variable.name = "Statistics")
 
 
 # deal with outliers
 mean.wcount = mean(d.wcount$wordCount)
-sd.wccount = sd(d.wcount$wordCount)
+sd.wcount = sd(d.wcount$wordCount)
+
+mean.ccount = mean(d.ccount$charCount)
+sd.ccount = sd(d.ccount$charCount)
+
+mean.vsize = mean(d.vsize$vocabSize)
+sd.vsize = sd(d.vsize$vocabSize)
+
+d.comb.clean  = d.comb %>%
+    filter(between(wordCount, mean.wcount - 2*sd.wcount, mean.wcount + 2*sd.wcount)) %>%
+    filter(between(charCount, mean.ccount - 2*sd.ccount, mean.ccount + 2*sd.ccount)) %>%
+    filter(between(vocabSize, mean.vsize - 2*sd.vsize, mean.vsize + 2*sd.vsize))
+d.comb.clean = data.table(d.comb.clean)
+d.combm.clean = melt(d.comb.clean, id.vars = "year", variable.name = "Statistics")
 
 
-
-### Scatterplot
-p.scatter = ggplot(d.combm[year<=1980], aes(x=year, y=value)) + 
+### Plot
+p.scatter = ggplot(d.combm.clean[year<=1950], aes(x=year, y=value)) + 
     geom_point(aes(color=Statistics, shape=Statistics)) + 
+    geom_smooth(aes(color=Statistics, linetype=Statistics))
+
+p.jitter = ggplot(d.combm.clean[year<=1950], aes(x=year, y=value)) + 
+    geom_jitter(aes(color=Statistics, shape=Statistics)) + 
+    geom_smooth(aes(color=Statistics, linetype=Statistics))
+
+p.smooth = ggplot(d.combm.clean[year<=1950], aes(x=year, y=value)) + 
+    geom_jitter(aes(color=Statistics, shape=Statistics)) + 
     geom_smooth(aes(color=Statistics, linetype=Statistics))
