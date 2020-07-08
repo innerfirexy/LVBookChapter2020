5df62408421b376fafd4947502db62ad5c108cb5
xy@工作間 <innerfirexy@gmail.com>
2020-07-08 11:28:46 -0700

    the train code works

--- train.py
+++ train.py
@@ -15,15 +15,20 @@ def run_CWE(bin_dir: str, data_dir: str,
         word_vec_file = os.path.join(grp, f'wordvec_cbow{cbow}_size{size}_cwetype{cwe_type}.txt')
         char_vec_file = os.path.join(grp, f'charvec_cbow{cbow}_size{size}_cwetype{cwe_type}.txt')
 
+        print(f'train {grp}')
         cmd = f'{bin_dir} -train {data_file} -output-word {word_vec_file} -output-char {char_vec_file} \
             -cbow {cbow} -size {size} -cwe-type {cwe_type}'
-        subprocess.Popen(cmd, shell=True)
-    pass
+        subprocess.run(cmd, shell=True)
+        print('done.')
 
 
 def main():
-    run_CWE(bin_dir = '/Users/xy/GitHub/CWE/src/cwe', 
-        data_dir = './data/group_year_span/100years_cutoff1951')
+    CWE_BIN_DIR = '/Users/xy/GitHub/CWE/src/cwe'
+
+    # 100 years per group
+    DATA_DIR1 = './data/group_year_span/100years_cutoff1951'
+    # run_CWE(bin_dir = CWE_BIN_DIR, data_dir = DATA_DIR1, cbow=1, size=300, cwe_type=1)
+    run_CWE(bin_dir = CWE_BIN_DIR, data_dir = DATA_DIR1, cbow=0, size=300, cwe_type=1)
 
 
 if __name__ == "__main__":
